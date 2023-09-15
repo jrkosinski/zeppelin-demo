8c3e2fd1bab6bf920bff207c064b973b22692bb6
Acer <john.kosinski@gmail.com>
2023-09-15 21:18:00 +0700

    removed warnings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React, { useState, useRef } from 'react'; 
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Sell from "./Sell";
